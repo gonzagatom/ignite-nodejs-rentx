1bdd4e17aa4d7ceb9c7b21b322a4e2fbd484ec53
GabrielRCodes <gabrielrcodes@gmail.com>
2022-07-11 09:22:37 -0300

    Adding specifications route

---
+++ src/modules/cars/routes/specifications.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import { CreateSpecificationService } from "../../../services/CreateSpecificationService";
+import { SpecificationsRepository } from "../repositories/SpecificationsRepository";
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationsRoutes };
