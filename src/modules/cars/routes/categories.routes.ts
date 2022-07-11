50da9971a0360b89c6eeadbab227f938533962bb
GabrielRCodes <gabrielrcodes@gmail.com>
2022-07-11 09:57:42 -0300

    Adding list controllers

--- src/modules/cars/routes/categories.routes.ts
+++ src/modules/cars/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
 import { createCategoryController } from "../useCases/createCategory";
+import { listCategoriesController } from "../useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.json(all);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
