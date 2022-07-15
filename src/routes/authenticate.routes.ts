e779e78f78d235d9633a7901ed0daee1ee6b52b7
GabrielRCodes <gabrielrcodes@gmail.com>
2022-07-15 11:43:07 -0300

    Adding token generation

--- src/routes/authenticate.routes.ts
+++ src/routes/authenticate.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 
+import { AuthenticateUserController } from "../modules/accounts/UseCases/authenticateUser/AuthenticateUserController";
+
 const authenticateRoutes = Router();
 
+const authenticateUserController = new AuthenticateUserController();
+
 // Criando rota de tipo postgres
-authenticateRoutes.post("/sessions");
+authenticateRoutes.post("/sessions", authenticateUserController.handle);
 
 export { authenticateRoutes };
