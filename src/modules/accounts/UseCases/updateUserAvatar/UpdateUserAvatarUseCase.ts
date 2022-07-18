d663f72fa6b4d04313849815d542e990494ead1a
GabrielRCodes <gabrielrcodes@gmail.com>
2022-07-18 10:49:37 -0300

    Adding UpdateUserAvatar

---
+++ src/modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,11 @@
+// Adicionar coluna avatar na tabela de users
+// Refatorar o usuário com coluna avatar
+// Configuração de upload do multer
+// Criar regra de negócio do upload
+// Criar controller
+
+class UpdateUserAvatarUseCase {
+  async execute() {}
+}
+
+export { UpdateUserAvatarUseCase };
