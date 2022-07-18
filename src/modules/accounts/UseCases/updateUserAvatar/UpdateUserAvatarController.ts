5dc7a8272500da790e95e585a4ebaf8eed61f5f6
GabrielRCodes <gabrielrcodes@gmail.com>
2022-07-18 11:17:18 -0300

    Adding update route

--- src/modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,5 +1,22 @@
-class UpdateUSerAvatarController {
-  async handle(request: Request, response: Response) {}
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
+
+class UpdateUserAvatarController {
+  async handle(request: Request, response: Response) {
+    const { id } = request.user;
+
+    // Receber arquivo
+
+    const avatar_file = null;
+
+    const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
+
+    await updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
+
+    return response.status(200).send();
+  }
 }
 
-export { UpdateUSerAvatarController };
+export { UpdateUserAvatarController };
